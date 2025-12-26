8810c792c2fb0bc63e8909016d0211fa0c0ecad9
Droun Trehan <106568215+maybedrone@users.noreply.github.com>
2025-12-26 20:51:17 +0530

    Update main.py
    fixing layout

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from register import *
 from bank import *
 
 status = False
-print("Welcome to Mohit Banking Project")
+print("Welcome to The Banking Project")
 while True:
     try:
         register = int(input("1. SignUp\n"
